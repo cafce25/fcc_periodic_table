c4450a06279acbcf279fa9d673c6a8d46be85b57
cafce25 <cafce25@gmail.com>
2022-04-19 13:38:30 +0000

    feat: lookup by atomic number

---
+++ element.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+PSQL="psql -X --user=freecodecamp --dbname=periodic_table --tuples-only -c"
+if [[ -z $1 ]]
+then
+  echo Please provide an element as an argument.
+  exit 1
+fi
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  ATOMIC_NUMBER=$1
+fi
+if [[ -z $ATOMIC_NUMBER ]]
+then
+  # lookup element by symbol
+  echo by symbol
+fi
+if [[ -z $ATOMIC_NUMBER ]]
+then
+  # lookup element by name
+  echo by name
+fi
+if [[ -z $ATOMIC_NUMBER ]]
+then
+  echo I could not find that element in the database.
+  exit 2
+else
+  # fectch all info
+  ELEMENT_INFO=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $ATOMIC_NUMBER")
+  # display info
+  echo $ELEMENT_INFO
+fi
